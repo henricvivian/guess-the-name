57cfb2c17bc160c1874efd59cc267f0138f18870
henricvivian <125461571+henricvivian@users.noreply.github.com>
2023-06-08 20:30:37 +0530

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,25 +1,46 @@
 import random
 
-def guess_the_number():
-    print("Welcome to Guess the Number!")
-    secret_number = random.randint(1, 100)
-    attempts = 0
+class NumberGuessingGame:
+    def __init__(self):
+        self.secret_number = None
+        self.attempts = 0
 
-    while True:
+    def start(self):
+        print("Welcome to Guess the Number!")
+        self.secret_number = random.randint(1, 100)
+        self.attempts = 0
+
+        while True:
+            self.attempts += 1
+            self.make_guess()
+
+            if self.is_guess_correct():
+                self.print_congratulations()
+                break
+
+            if self.is_max_attempts_reached():
+                self.print_attempts_limit_reached()
+                break
+
+    def make_guess(self):
         guess = int(input("Guess a number between 1 and 100: "))
-        attempts += 1
-
-        if guess < secret_number:
+        if guess < self.secret_number:
             print("Too low!")
-        elif guess > secret_number:
+        elif guess > self.secret_number:
             print("Too high!")
-        else:
-            print(f"Congratulations! You guessed the number in {attempts} attempts!")
-            break
 
-        if attempts >= 5:
-            print("Sorry, you have run out of attempts.")
-            break
+    def is_guess_correct(self):
+        return self.secret_number == guess
+
+    def print_congratulations(self):
+        print(f"Congratulations! You guessed the number in {self.attempts} attempts!")
+
+    def is_max_attempts_reached(self):
+        return self.attempts >= 5
+
+    def print_attempts_limit_reached(self):
+        print("Sorry, you have run out of attempts.")
 
 if __name__ == "__main__":
-    guess_the_number()
+    game = NumberGuessingGame()
+    game.start()
